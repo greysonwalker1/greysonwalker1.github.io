a5d0adfe52582ef3571f8acaa22a084c51948103
greysonwalker1 <greyson.walker@alpha.school>
2025-01-17 15:01:32 -0600

    Update map.js

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 const options = {
     // Required: API key
-    key: 'UN0v0SoLtWOoMULzpJqKeqEt5gnbuW94', // REPLACE WITH YOUR KEY !!!
+    key: 'zLFEKEo1xSyBoopJEZClLSa1Sp4mfNe9', // REPLACE WITH YOUR KEY !!!
 
     // Put additional console output
     verbose: true,
